0a311a163ea7ff7f1dec4130aad27712de8acabf
giulitz189 <klaimer671@gmail.com>
2019-04-07 15:48:17 +0200

    Eliminato il gitignore per il testing in locale

---
+++ local_testing/script.js
@@ -0,0 +1,65 @@
+var width = 1000,
+	height = 500;
+	
+var map = d3.select(".map")
+	.attr("preserveAspectRatio", "xMinYMin meet")
+	.attr("viewBox", "0 0 " + width + " " + height)
+	.append("g");
+	
+var projection = d3.geoMercator()
+	.translate([width/2, height/2])
+	.center([12, 42.3])
+	.scale(1950);
+	
+var path = d3.geoPath()
+	.projection(projection);
+	
+var mapData = d3.json("http://127.0.0.1:8765/geodata/italy_reg.json");
+var queryData = d3.tsv("http://127.0.0.1:8765/query.tsv", type);
+
+Promise.all([mapData, queryData]).then(function(data) {
+	function stringToPoint(str) {
+		var res = str.split(" ");
+		var x_str = res[0].split("Point(");
+		var y_str = res[1].split(")");
+		var x = +x_str[1];
+		var y = +y_str[0];
+		return [x, y];
+	};
+	
+	// draw map
+	map.selectAll("path")
+		.data(topojson.feature(data[0], data[0].objects.sub).features)
+		.enter()
+		.append("path")
+			.attr("class", "region")
+			.attr("d", path),
+		
+	// draw point
+	map.selectAll("circle")
+		.data(data[1])
+		.enter()
+		.append("circle")
+			.attr("class", "circles")
+			.attr("cx", function(d) {
+				var point = stringToPoint(d.coord);
+				return projection(point)[0];
+			})
+			.attr("cy", function(d) {
+				var point = stringToPoint(d.coord);
+				return projection(point)[1];
+			})
+			.attr("r", "1px")
+			.attr("fill", function(d) {
+				switch (d.genderLabel) {
+					case "maschio": return "#0099FF";
+					case "femmina": return "#FF00FF";
+					default: return "#66FF66";
+				}
+			});
+});
+
+function type(d) {
+	d.age = +d.age;
+	return d;
+}
