2eb7bd527e72815c637b5bb213266f4cdfbe2c37
giulitz189 <klaimer671@gmail.com>
2019-09-06 19:40:10 +0200

    Query modified, data raised to about 62.000 records

--- query_records/preprocessor.js
+++ query_records/preprocessor.js
@@ -19,182 +19,226 @@ class SPARQLQueryDispatcher {
 	}
 }
 
+// Wikidata Query Service endpoint
 const ENDPOINT_URL = 'https://query.wikidata.org/sparql';
+// Regular expression for Wikidata entity codes
+const WIKIDATA_OBJECT_CODE_REGEXP = /Q[0-9]+/;
 
-// Query for living people (Source link with comments: https://bit.ly/2ykz2ef)
-const QUERY_LIVING_PEOPLE = 'SELECT ?persona ?personaLabel ?genderLabel ?occupazioneLabel (YEAR(?dob) AS ?anno) ?pobLabel ?coord WITH {\
-	SELECT DISTINCT ?persona ?dob WHERE {\
-	  ?persona wdt:P1412 wd:Q652;\
-				wdt:P27 wd:Q38;\
-				 wdt:P31 wd:Q5.\
-	  FILTER EXISTS { ?persona wdt:P569 ?data_nascita. }\
-	  FILTER NOT EXISTS { ?persona wdt:P570 ?data_morte. }\
-	  ?articolo schema:about ?persona.\
-	  FILTER (SUBSTR(str(?articolo), 16, 5) = "pedia").\
-	  ?persona wdt:P569 ?dob.\
-	  BIND(YEAR(now()) - YEAR(?dob) as ?age)\
-	  FILTER(?age <= 110)\
-	}\
-  } AS %i WHERE {\
-	include %i\
-	?persona wdt:P21 ?gender;\
-			 wdt:P106 ?occupazione;\
-			 wdt:P19 ?pob.\
-	?pob wdt:P625 ?coord.\
-	SERVICE wikibase:label { bd:serviceParam wikibase:language "it,en". }\
-  } ORDER BY ?personaLabel';
+// Fetch function for living people data retrieval: here will be sended multiple queries, one for every 25-year interval
+// (Source: https://w.wiki/7yj)
+function fetchLivingPeople(queryDispatcher) {
+	var result = { post_records: [] };
+	
+	for (var age_min = 0; age_min < 100; age_min += 25) {
+		var age_max = (age_min + 25 < 100) ? (age_min + 25 - 1) : 110;
+		var query_string = 'SELECT ?persona ?personaLabel ?genderLabel ?occupazioneLabel (YEAR(?dob) AS ?anno) ?pobLabel ?coord WITH {\
+			SELECT DISTINCT ?persona ?dob WHERE {\
+				?persona wdt:P27 wd:Q38;\
+						     wdt:P31 wd:Q5.\
+				FILTER EXISTS { ?persona wdt:P569 ?data_nascita. }\
+				FILTER NOT EXISTS { ?persona wdt:P570 ?data_morte. }\
+				?articolo schema:about ?persona.\
+				FILTER (SUBSTR(str(?articolo), 16, 5) = "pedia").\
+				?persona wdt:P569 ?dob.\
+				BIND(YEAR(now()) - YEAR(?dob) as ?age)\
+				FILTER(?age > ' + age_min + ' && ?age <= ' + age_max + ').\
+			}\
+		} AS %i WHERE {\
+			include %i\
+			?persona wdt:P21 ?gender;\
+					 wdt:P106 ?occupazione;\
+					 wdt:P19 ?pob.\
+			?pob wdt:P625 ?coord.\
+			SERVICE wikibase:label { bd:serviceParam wikibase:language "it,en". }\
+		} ORDER BY ?personaLabel';
+		
+		var fetchedData = queryDispatcher.query(query_string);
+		result.post_records.push(fetchedData);
+	}
 
-// Query for dead people (Source link with comments: https://bit.ly/2GAjCXE)
-const QUERY_DEAD_PEOPLE = 'SELECT ?persona ?personaLabel ?genderLabel ?occupazioneLabel (YEAR(?dob) AS ?anno_nascita) (YEAR(?dod) AS ?anno_morte) ?pobLabel ?coord WITH {\
-	SELECT DISTINCT ?persona ?dob ?dod WHERE {\
-	  ?persona wdt:P1412 wd:Q652;\
-				 wdt:P27 wd:Q38;\
-			   wdt:P31 wd:Q5.\
-	  FILTER EXISTS { ?persona wdt:P569 ?data_nascita. }\
-	  FILTER EXISTS { ?persona wdt:P570 ?data_morte. }\
-	  ?articolo schema:about ?persona.\
-	  FILTER (SUBSTR(str(?articolo), 16, 5) = "pedia").\
-	  ?persona wdt:P569 ?dob.\
-	  hint:Prior hint:rangeSafe true.\
-	  ?persona wdt:P570 ?dod.\
-	  hint:Prior hint:rangeSafe true.\
-	  FILTER(?dob >= "1850-00-00"^^xsd:dateTime && ?dod >= "1850-00-00"^^xsd:dateTime).\
-	}\
-  } AS %i WHERE {\
-	include %i\
-	?persona wdt:P21 ?gender;\
-			 wdt:P106 ?occupazione;\
-			 wdt:P19 ?pob.\
-	?pob wdt:P625 ?coord.\
-	SERVICE wikibase:label { bd:serviceParam wikibase:language "it,en". }\
-  } ORDER BY ?personaLabel';
+	return result;
+}
 
-// Data fetch phase: people lists are fetched via promise functions, occupation lists
-// are read by Node.js file system module
-console.log('Fetching data...');
+// Fetch function for dead people data retrieval: here will be sended multiple queries, one for every 25-year interval
+// (Source: https://bit.ly/2m1fLeJ)
+function fetchDeadPeople(queryDispatcher) {
+	var result = { post_records: [] };
+	
+	for (var year_min = 1850; year_min < 2019; year_min += 25) {
+		var year_max = (year_min + 25 > 2019) ? 2019 : (year_min + 25);
+		var query_string = 'SELECT ?persona ?personaLabel ?genderLabel ?occupazioneLabel (YEAR(?dob) AS ?anno_nascita) (YEAR(?dod) AS ?anno_morte) ?pobLabel ?coord WITH {\
+			SELECT DISTINCT ?persona ?dob ?dod WHERE {\
+				?persona wdt:P27 wd:Q38;\
+								 wdt:P31 wd:Q5.\
+				FILTER EXISTS { ?persona wdt:P569 ?data_nascita. }\
+				FILTER EXISTS { ?persona wdt:P570 ?data_morte. }\
+				?articolo schema:about ?persona.\
+				FILTER (SUBSTR(str(?articolo), 16, 5) = "pedia").\
+				?persona wdt:P569 ?dob.\
+				hint:Prior hint:rangeSafe true.\
+				?persona wdt:P570 ?dod.\
+				hint:Prior hint:rangeSafe true.\
+				FILTER(?dob >= "' + year_min + '-00-00"^^xsd:dateTime\
+							 && ?dob < "' + year_max + '-00-00"^^xsd:dateTime\
+							 && ?dod >= "1850-00-00"^^xsd:dateTime).\
+			}\
+		} AS %i WHERE {\
+			include %i\
+			?persona wdt:P21 ?gender;\
+							 wdt:P106 ?occupazione;\
+							 wdt:P19 ?pob.\
+			?pob wdt:P625 ?coord.\
+			SERVICE wikibase:label { bd:serviceParam wikibase:language "it,en". }\
+		} ORDER BY ?personaLabel';
+		
+		var fetchedData = queryDispatcher.query(query_string);
+		result.post_records.push(fetchedData);
+	}
+
+	return result;
+}
+
 const queryDispatcher_ = new SPARQLQueryDispatcher(ENDPOINT_URL);
-var livingPeopleFetchedData = queryDispatcher_.query(QUERY_LIVING_PEOPLE);
-var deadPeopleFetchedData = queryDispatcher_.query(QUERY_DEAD_PEOPLE);
-
 var queryResultsObject = {
 	'results': []
 };
 
+// Data fetch phase: people lists are fetched via promise functions, occupation lists
+// are read by Node.js file system module
+console.log('Fetching profession data...');
 fs.readFile('professions.json', (err, data) => {
 	if (err) throw err;
 	var professionList = JSON.parse(data);
+
+	// Geographical Mercator projection is precalculated here in order to avoid
+	// heavy calcs during page rendering
+	var viewBoxMapCoordinates = {
+		x: 0,
+		y: 0,
+		width: 1000,
+		height: 500
+	};
 	
-	Promise.all([livingPeopleFetchedData, deadPeopleFetchedData]).then(function(data) {
-		var livingPeopleValues = data[0].results.bindings;
-		var deadPeopleValues = data[1].results.bindings;
-		
-		console.log('Generating records...');
-		
-		// Geographical Mercator projection is precalculated here in order to avoid
-		// heavy calcs during page rendering
-		var viewBoxMapCoordinates = {
-			x: 0,
-			y: 0,
-			width: 1000,
-			height: 500
-		};
-		
-		var projection = d3.geoMercator()
-			.translate([viewBoxMapCoordinates.width/2, viewBoxMapCoordinates.height/2])
-			.center([12, 42.1])
-			.scale(1950);
-		
+	var projection = d3.geoMercator()
+		.translate([viewBoxMapCoordinates.width/2, viewBoxMapCoordinates.height/2])
+		.center([12, 42.1])
+		.scale(1950);
+
+	console.log('Living people: fetching...');
+	var livingPeopleFetchedData = fetchLivingPeople(queryDispatcher_);
+	var livingPeopleRecordsGroup = livingPeopleFetchedData.post_records;
+	
+	Promise.all(livingPeopleRecordsGroup).then(function(groupData1) {
+		console.log('Living people: generating records...');
+
 		// Generating points for living people...
 		var previousPersonName = '';
-		for (i = 0; i < livingPeopleValues.length; i++) {
-			var occupationValueList = livingPeopleValues[i].occupazioneLabel.value;
-			var categories = getCategories(occupationValueList, professionList);
-			if (livingPeopleValues[i].personaLabel.value != previousPersonName) {
-				// professions object contains occupation labels for every person and
-				// their respective categories
-				var professions = {
-					'categories': categories,
-					'occupations': []
-				};
-				var obj = {
-					'wiki_id': livingPeopleValues[i].persona.value.split('http://www.wikidata.org/entity/')[1],
-					'name': livingPeopleValues[i].personaLabel.value,
-					'gender': livingPeopleValues[i].genderLabel.value,
-					'professions': professions,
-					'dob': toInteger(livingPeopleValues[i].anno.value),
-					'dod': 0,
-					'pob': livingPeopleValues[i].pobLabel.value,
-					'coords': toPointObj(livingPeopleValues[i].coord.value, projection)
-				};
+		for (group_idx = 0, len = groupData1.length; group_idx < len; group_idx++) {
+			var livingPeopleValues = groupData1[group_idx].results.bindings;
+			for (i = 0; i < livingPeopleValues.length; i++) {
+				if (!WIKIDATA_OBJECT_CODE_REGEXP.test(livingPeopleValues[i].personaLabel.value)) {
+					var occupationValueList = livingPeopleValues[i].occupazioneLabel.value;
+					var categories = getCategories(occupationValueList, professionList);
+					if (livingPeopleValues[i].personaLabel.value != previousPersonName) {
+						// professions object contains occupation labels for every person and
+						// their respective categories
+						var professions = {
+							'categories': categories,
+							'occupations': []
+						};
+						var obj = {
+							'wiki_id': livingPeopleValues[i].persona.value.split('http://www.wikidata.org/entity/')[1],
+							'name': livingPeopleValues[i].personaLabel.value,
+							'gender': livingPeopleValues[i].genderLabel.value,
+							'professions': professions,
+							'dob': toInteger(livingPeopleValues[i].anno.value),
+							'dod': 0,
+							'pob': livingPeopleValues[i].pobLabel.value,
+							'coords': toPointObj(livingPeopleValues[i].coord.value, projection)
+						};
 
-				obj.professions.occupations.push(livingPeopleValues[i].occupazioneLabel.value);
-				queryResultsObject.results.push(obj);
-				previousPersonName = livingPeopleValues[i].personaLabel.value;
-			} else {
-				// query records in Wikidata are registered singularly for each
-				// occupation value, so if someone is already inserted in results
-				// array we'll only add the new occupation value for this person
-				var last = queryResultsObject.results.length - 1;
-				var currCat = queryResultsObject.results[last].professions.categories;
-				for (j = 0; j < categories.length; j++) {
-					if (currCat.findIndex(val => val == categories[j]) < 0)
-						queryResultsObject.results[last].professions.categories.push(categories[j]);
+						obj.professions.occupations.push(livingPeopleValues[i].occupazioneLabel.value);
+						queryResultsObject.results.push(obj);
+						previousPersonName = livingPeopleValues[i].personaLabel.value;
+					} else {
+						// query records in Wikidata are registered singularly for each
+						// occupation value, so if someone is already inserted in results
+						// array we'll only add the new occupation value for this person
+						var last = queryResultsObject.results.length - 1;
+						var currCat = queryResultsObject.results[last].professions.categories;
+						for (j = 0; j < categories.length; j++) {
+							if (currCat.findIndex(val => val == categories[j]) < 0)
+								queryResultsObject.results[last].professions.categories.push(categories[j]);
+						}
+						queryResultsObject.results[last].professions.occupations.push(livingPeopleValues[i].occupazioneLabel.value);
+					}
 				}
-				queryResultsObject.results[last].professions.occupations.push(livingPeopleValues[i].occupazioneLabel.value);
 			}
 		}
-		
-		// ... and dead ones
-		previousPersonName = '';
-		for (i = 0; i < deadPeopleValues.length; i++) {
-			if (deadPeopleValues[i].personaLabel.value != previousPersonName) {
-				var occupationValueList = deadPeopleValues[i].occupazioneLabel.value;
-				var categories = getCategories(occupationValueList, professionList);
-				
-				var professions = {
-					'categories': categories,
-					'occupations': []
-				};
-				var obj = {
-					'wiki_id': deadPeopleValues[i].persona.value.split('http://www.wikidata.org/entity/')[1],
-					'name': deadPeopleValues[i].personaLabel.value,
-					'gender': deadPeopleValues[i].genderLabel.value,
-					'professions': professions,
-					'dob': toInteger(deadPeopleValues[i].anno_nascita.value),
-					'dod': toInteger(deadPeopleValues[i].anno_morte.value),
-					'pob': deadPeopleValues[i].pobLabel.value,
-					'coords': toPointObj(deadPeopleValues[i].coord.value, projection)
-				};
-				
-				obj.professions.occupations.push(deadPeopleValues[i].occupazioneLabel.value);
-				queryResultsObject.results.push(obj);
-				previousPersonName = deadPeopleValues[i].personaLabel.value;
-			} else {
-				var last = queryResultsObject.results.length - 1;
-				var currCat = queryResultsObject.results[last].professions.categories;
-				for (j = 0; j < categories.length; j++) {
-					if (currCat.findIndex(val => val == categories[j]) < 0)
-						queryResultsObject.results[last].professions.categories.push(categories[j]);
+
+		console.log('Dead people: fetching...');
+		var deadPeopleFetchedData = fetchDeadPeople(queryDispatcher_);
+		var deadPeopleRecordsGroup = deadPeopleFetchedData.post_records;
+
+		Promise.all(deadPeopleRecordsGroup).then(function(groupData2) {
+			console.log('Dead people: generating records...');
+
+			// ... and dead ones
+			previousPersonName = '';
+			for (group_idx = 0, len = groupData2.length; group_idx < len; group_idx++) {
+				var deadPeopleValues = groupData2[group_idx].results.bindings;
+				for (i = 0; i < deadPeopleValues.length; i++) {
+					if (!WIKIDATA_OBJECT_CODE_REGEXP.test(deadPeopleValues[i].personaLabel.value)) {
+						var occupationValueList = deadPeopleValues[i].occupazioneLabel.value;
+						var categories = getCategories(occupationValueList, professionList);
+						if (deadPeopleValues[i].personaLabel.value != previousPersonName) {
+							var professions = {
+								'categories': categories,
+								'occupations': []
+							};
+							var obj = {
+								'wiki_id': deadPeopleValues[i].persona.value.split('http://www.wikidata.org/entity/')[1],
+								'name': deadPeopleValues[i].personaLabel.value,
+								'gender': deadPeopleValues[i].genderLabel.value,
+								'professions': professions,
+								'dob': toInteger(deadPeopleValues[i].anno_nascita.value),
+								'dod': toInteger(deadPeopleValues[i].anno_morte.value),
+								'pob': deadPeopleValues[i].pobLabel.value,
+								'coords': toPointObj(deadPeopleValues[i].coord.value, projection)
+							};
+							
+							obj.professions.occupations.push(deadPeopleValues[i].occupazioneLabel.value);
+							queryResultsObject.results.push(obj);
+							previousPersonName = deadPeopleValues[i].personaLabel.value;
+						} else {
+							var last = queryResultsObject.results.length - 1;
+							var currCat = queryResultsObject.results[last].professions.categories;
+							for (j = 0; j < categories.length; j++) {
+								if (currCat.findIndex(val => val == categories[j]) < 0)
+									queryResultsObject.results[last].professions.categories.push(categories[j]);
+							}
+							queryResultsObject.results[last].professions.occupations.push(deadPeopleValues[i].occupazioneLabel.value);
+						}
+					}
 				}
-				queryResultsObject.results[last].professions.occupations.push(deadPeopleValues[i].occupazioneLabel.value);
 			}
-		}
-		
-		// sorting alphabetically by name
-		console.log('Sorting records by name...');
-		queryResultsObject.results.sort(function (a, b) {
-			a = a.name.toLowerCase();
-			b = b.name.toLowerCase();
-			return a < b ? -1 : a > b ? 1 : 0;
-		});
-		
-		// save on json file
-		console.log('Saving...');
-		var serializedData = JSON.stringify(queryResultsObject);
-		fs.writeFile('query_results.json', serializedData, function(err) {
-			if (err) throw err;
-			console.log('Data retrieved successfully!');
-		});
+			
+			// sorting alphabetically by name
+			console.log('Sorting records by name...');
+			queryResultsObject.results.sort(function (a, b) {
+				a = a.name.toLowerCase();
+				b = b.name.toLowerCase();
+				return a < b ? -1 : a > b ? 1 : 0;
+			});
+			
+			// save on json file
+			console.log('Saving...');
+			var serializedData = JSON.stringify(queryResultsObject);
+			fs.writeFile('query_results.json', serializedData, function(err) {
+				if (err) throw err;
+				console.log('Data retrieved successfully!');
+			});
+		}).catch(function(e) { console.log(e); });
 	}).catch(function(e) { console.log(e); });
 });
 
